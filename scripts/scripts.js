c8642a13f0f5471c2f17abb936429d8665c3da16
Brian Elliott <ellio2bj@dukes.jmu.edu>
2022-05-18 21:07:08 -0400

    Prototyping game board

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -4,3 +4,26 @@ const GamePlayer = (name) => {
 
     return {name, score}
 }
+
+const submitButton = document.querySelector("input[type='submit']")
+const gameBox = document.querySelectorAll(".game-box")
+let toggleValue = -1;
+
+function toggleDOM(e) {
+    e.preventDefault()
+    const gameContainer = document.querySelector(".game-container")
+
+    const value = gameContainer.style.getPropertyValue("visibility") === "visible" ? "hidden": "visible"
+    gameContainer.style.setProperty("visibility", value)
+}
+
+function selectRandomLetter(e) {
+    e.target.textContent = toggleValue === 1 ? "X" : "O"
+    toggleValue *= -1
+}
+
+gameBox.forEach(() => {
+   this.addEventListener("click", selectRandomLetter)
+});
+
+submitButton.addEventListener("click", toggleDOM)
